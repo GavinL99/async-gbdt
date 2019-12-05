30e7a0647dfbd963b57392ff836106b0616f78b1
GavinL99 <liugf_99@163.com>
2019-12-05 18:20:47 -0500

    use concurrent list instead of queue for maintaining the forest

--- src/cpp/concurrency.h
+++ src/cpp/concurrency.h
@@ -18,13 +18,17 @@ namespace gbdt {
  * Single writer, multiple readers
  * Writer has higher priority, so further readers will block
  */
+  using mutex_t = std::mutex;
+  using cond_t = std::condition_variable;
+  using uniq_lock = std::unique_lock<std::mutex>;
+
   class ReaderWriterLatch {
-    using mutex_t = std::mutex;
-    using cond_t = std::condition_variable;
 
   public:
     ReaderWriterLatch() = default;
-    ~ReaderWriterLatch() { std::lock_guard<mutex_t> guard(mutex_); }
+    ~ReaderWriterLatch() {
+      uniq_lock latch(mutex_);
+    }
 
     DISALLOW_COPY_AND_ASSIGN(ReaderWriterLatch);
 
@@ -32,7 +36,7 @@ namespace gbdt {
      * Acquire a write latch.
      */
     void WLock() {
-      std::unique_lock<mutex_t> latch(mutex_);
+      uniq_lock latch(mutex_);
       writer_entered_ = true;
       while (reader_count_ > 0) {
         writer_.wait(latch);
@@ -43,7 +47,7 @@ namespace gbdt {
      * Release a write latch.
      */
     void WUnlock() {
-      std::lock_guard<mutex_t> guard(mutex_);
+      uniq_lock guard(mutex_);
       writer_entered_ = false;
       reader_.notify_all();
     }
@@ -52,7 +56,7 @@ namespace gbdt {
      * Acquire a read latch.
      */
     void RLock() {
-      std::unique_lock<mutex_t> latch(mutex_);
+      uniq_lock latch(mutex_);
       while (writer_entered_) {
         reader_.wait(latch);
       }
@@ -63,7 +67,7 @@ namespace gbdt {
      * Release a read latch.
      */
     void RUnlock() {
-      std::lock_guard<mutex_t> guard(mutex_);
+      uniq_lock guard(mutex_);
       reader_count_--;
       if (writer_entered_) {
         if (reader_count_ == 0) {
@@ -80,6 +84,44 @@ namespace gbdt {
     bool writer_entered_{false};
   };
 
+
+  /*
+   * The good thing about a list is we can maintain the forest in place!
+   */
+  template <Typename T>
+  class ConcurrentList {
+  public:
+    ConcurrentQueue(): processed_(0) {}
+
+    ~ConcurrentQueue() {
+      for (T* t: list_) {
+        delete t;
+      }
+    }
+
+    void push_and_notify(const uniq_p<T>& data) {
+      uniq_lock latch(latch_);
+      list_.push_back(std::move(data));
+      if (list_.size() == processed_ + 1) {
+        latch.unlock();
+        cv_.notify_one();
+      }
+    }
+
+    T* wait_and_consume() {
+      uniq_lock latch(latch_);
+      while (list_.size() == processed_) {
+        cv_.wait(latch);
+      }
+      return list_[processed_++];
+    }
+
+  private:
+    std::list<T*> list_;
+    mutex_t latch_;
+    cond_t cv_;
+    int processed_;
+  };
 
 
 
