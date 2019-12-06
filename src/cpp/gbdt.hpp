915781ad4ab86f878842292e355c8cdfeabf3e5d
GavinL99 <liugf_99@163.com>
2019-12-06 18:57:50 -0500

    fix bugs for shared mem implementation

--- src/cpp/gbdt.hpp
+++ src/cpp/gbdt.hpp
@@ -1,73 +1,78 @@
 // Author: qiyiping@gmail.com (Yiping Qi)
 #ifndef _GBDT_H_
 #define _GBDT_H_
+
 #include "tree_seq.hpp"
 #include "concurrency.h"
 
 namespace gbdt {
-class GBDT {
- public:
-  GBDT(Configure conf): trees(NULL),
-                        bias(0),
-                        conf(conf),
-                        gain(NULL) {
-    shrinkage = conf.shrinkage;
-    iterations = conf.iterations;
-  }
-
-  void Fit_OMP(DataVector *d);
-
-  void Fit_Async(DataVector *d, int threads_wanted);
-
-  std::string Save() const;
-  void Load(const std::string &s);
-
-  double *GetGain() { return gain; }
-
-  ValueType Predict(const Tuple &t) const;
-
-  ~GBDT();
- private:
-  void Init(DataVector &d);
-  double GetLoss(DataVector *d, size_t samples, int i, ValueType* temp_pred);
-
-  ValueType Predict_OMP(const Tuple &t, size_t n, ValueType temp_pred) const;
-  ValueType PredictAsync(const Tuple &t, RegressionTree *tree, ValueType temp_pred) const;
-
-  void WorkerSide();
-  void ServerSide(int dsize, int iter, std::vector<ValueType>& temp_pred);
-
-
-
-  void ReleaseTrees() {
-    if (trees) {
-      for (size_t i = 0; i < iterations; ++i) {
-        delete trees[i];
-      }
-      delete[] trees;
-      trees = NULL;
+  class GBDT {
+  public:
+    GBDT(Configure conf) : trees(NULL),
+                           bias(0),
+                           conf(conf),
+                           gain(NULL) {
+      shrinkage = conf.shrinkage;
+      iterations = conf.iterations;
     }
-  }
 
- private:
-  RegressionTree **trees;
-  // for trees
-  ValueType bias;
-  ValueType shrinkage;
-  size_t iterations;
+    void Fit_OMP(DataVector *d);
 
-  // for async concurrency
-  DataVector* data_ptr_;
-  ReaderWriterLatch data_ptr_lock_;
-  ConcurrentVector<RegressionTree> trees_vec_;
-  bool server_finish_{false};
+    void Fit_Async(DataVector *d, int threads_wanted);
 
-  Configure conf;
+    std::string Save() const;
 
-  double *gain;
+    void Load(const std::string &s);
 
-  DISALLOW_COPY_AND_ASSIGN(GBDT);
-};
+    double *GetGain() { return gain; }
+
+    ValueType Predict(const Tuple &t) const;
+
+    ~GBDT();
+
+  private:
+    void Init(DataVector &d);
+
+    double GetLoss(DataVector *d, size_t samples, int i, ValueType *temp_pred);
+
+    ValueType Predict_OMP(const Tuple &t, size_t n, ValueType temp_pred) const;
+
+    ValueType PredictAsync(const Tuple &t, RegressionTree *tree, ValueType temp_pred) const;
+
+    void WorkerSide(int dsize);
+
+    void ServerSide(int dsize, int iter, std::vector <ValueType> &temp_pred, int num_threads);
+
+
+    void ReleaseTrees() {
+      if (trees) {
+        for (size_t i = 0; i < iterations; ++i) {
+          delete trees[i];
+        }
+        delete[] trees;
+        trees = NULL;
+      }
+    }
+
+  private:
+    RegressionTree **trees;
+    // for trees
+    ValueType bias;
+    ValueType shrinkage;
+    size_t iterations;
+
+    // for async concurrency
+    DataVector *data_ptr_;
+    ReaderWriterLatch data_ptr_lock_;
+    ConcurrentVector<RegressionTree> trees_vec_;
+    bool server_finish_{false};
+
+    Configure conf;
+
+    double *gain;
+
+    DISALLOW_COPY_AND_ASSIGN(GBDT);
+  };
 }
 
 #endif /* _GBDT_H_ */
